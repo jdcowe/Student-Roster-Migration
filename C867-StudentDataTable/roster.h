f6c7af5ac1d9fbab40f6021bd1d69f3ef403522f
Jordan Cowe <jordan@csnow.io>
2019-01-08 18:05:22 -0500

    Add project files.

---
+++ C867-StudentDataTable/roster.h
@@ -0,0 +1,10 @@
+#pragma once
+
+const string studentData[] =
+{
+	"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+	"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+	"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+	"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+	"A5,Jordan,Cowe,jcowe@wgu.edu,28,14,21,22,SOFTWARE"
+}
